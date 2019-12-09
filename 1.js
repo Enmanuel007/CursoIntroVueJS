0b35c8198bbbaf9cba0a77e894f03eb94eab450f
Enmanuel <eochoa@umariana.edu.co>
2019-12-09 03:47:17 -0500

    Agregar un if cuando el número de frutas sea = 0

--- 1.js
+++ 1.js
@@ -4,7 +4,7 @@ const app = new  Vue({
         titulo:'Hola mundo con Vue',
         frutas: [
             {nombre: 'manzana', cantidad: 10},
-            {nombre: 'pera', cantidad: 5},
+            {nombre: 'pera', cantidad: 0},
             {nombre: 'uva', cantidad: 3}
         ]
        
