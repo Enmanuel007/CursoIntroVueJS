01f26996de46bfcc44b7dbe3963a005d848482d7
Enmanuel <eochoa@umariana.edu.co>
2019-12-09 02:55:37 -0500

    ejecución de for a objetos en en el js

--- 1.js
+++ 1.js
@@ -2,7 +2,12 @@ const app = new  Vue({
     el: '#app',
     data: {
         titulo:'Hola mundo con Vue',
-        frutas: ['manzana', 'pera','plátano']
+        frutas: ['manzana', 'pera','plátano'],
+        ropa: [
+            {camisa: 'roja', cantidad: 10},
+            {pantalon: 'jean', cantidad: 5},
+            {sombrero: 'cuero', cantidad: 3}
+        ]
        
     }
 })
