d1327670d2164f71acd12d16219b23487cc47b42
Enmanuel <eochoa@umariana.edu.co>
2019-12-09 05:27:17 -0500

    Agregar 1 clase danger de boostrap

--- 1.js
+++ 1.js
@@ -1,36 +1,10 @@
 const app = new  Vue({
     el: '#app',
     data: {
-        titulo:'Hola mundo de Vue',
-        frutas: [
-            {nombre: 'manzana', cantidad: 10},
-            {nombre: 'pera', cantidad: 0},
-            {nombre: 'uva', cantidad: 3}
-        ],
 
-        nuevafruta: '',
-        total: 0
     },
 
-    
     methods:{
-        agregarFruta (){
-            this.frutas.push({
-                nombre: this.nuevafruta,
-                cantidad: 0
-            });
-            this.nuevafruta = '';
-        }
-    },
 
-
-    computed:{
-        sumarfrutas(){
-            this.total = 0;
-            for (frutaObjeto of this.frutas){
-                this.total = this.total + frutaObjeto.cantidad;
-            }
-            return this.total;
-        }
     }
 })
